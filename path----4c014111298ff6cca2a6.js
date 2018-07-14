b217be778cb8a3dff8a11a27e31cf2917bb56381
Vlad Stoica <vlad96stoica@gmail.com>
2018-07-14 15:55:08 +0300

    Updates

--- path----4c014111298ff6cca2a6.js
+++ path----4c014111298ff6cca2a6.js
@@ -1,2 +1,2 @@
-webpackJsonp([60335399758886],{114:function(a,t){a.exports={data:{site:{siteMetadata:{title:"Vlad Stoica",avatar:"http://gravatar.com/avatar/32ca7438bc506ad3ca9b3d3c2210d5f5?s=400",navigation:[{path:"/",name:"home"},{path:"/work",name:"work"},{path:"/education",name:"education"}]}}},layoutContext:{}}}});
+webpackJsonp([60335399758886],{109:function(a,t){a.exports={data:{site:{siteMetadata:{title:"Vlad Stoica",avatar:"http://gravatar.com/avatar/32ca7438bc506ad3ca9b3d3c2210d5f5?s=400",navigation:[{path:"/",name:"home"},{path:"/work",name:"work"},{path:"/education",name:"education"}]}}},layoutContext:{}}}});
 //# sourceMappingURL=path----4c014111298ff6cca2a6.js.map
