9460c669c3630aa64227991f7807c84de9956dae
Vlad Stoica <vlad96stoica@gmail.com>
2018-11-09 08:35:50 +0000

    S

--- src/components/layout/page_layout.tsx
+++ src/components/layout/page_layout.tsx
@@ -4,42 +4,50 @@ import Helmet from "react-helmet";
 import Avatar from "../header/avatar";
 import Navigation from "../header/navigation";
 import { CentredRow, Container } from "./grid";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  a {
+    color: red;
+  }
+`;
 
 const PageLayout = (props: { children: React.ReactNode }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-            avatar
-            navigation {
-              path
-              name
-            }
-          }
-        }
-      }
-    `}
-    // tslint:disable-next-line:jsx-no-lambda
-    render={data => (
-      <>
-        <Helmet title={data.site.siteMetadata.title} />
-        <Container>
-          <CentredRow>
-            <Avatar
-              imageUrl={data.site.siteMetadata.avatar}
-              name={data.site.siteMetadata.title}
-            />
-          </CentredRow>
+  <Container>{props.children}</Container>
+  // <StaticQuery
+  //   query={graphql`
+  //     query SiteTitleQuery {
+  //       site {
+  //         siteMetadata {
+  //           title
+  //           avatar
+  //           navigation {
+  //             path
+  //             name
+  //           }
+  //         }
+  //       }
+  //     }
+  //   `}
+  //   // tslint:disable-next-line:jsx-no-lambda
+  //   render={data => (
+  //     <>
+  //       <Helmet title={data.site.siteMetadata.title} />
+  //       <Container>
+  //         <CentredRow>
+  //           <Avatar
+  //             imageUrl={data.site.siteMetadata.avatar}
+  //             name={data.site.siteMetadata.title}
+  //           />
+  //         </CentredRow>
 
-          <Navigation links={data.site.siteMetadata.navigation} />
+  //         <Navigation links={data.site.siteMetadata.navigation} />
 
-          {props.children}
-        </Container>
-      </>
-    )}
-  />
+  //         {props.children}
+  //       </Container>
+  //     </>
+  //   )}
+  // />
 );
 
 export default PageLayout;
